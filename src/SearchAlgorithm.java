1fc61f8ee7ecacbe8795f712625028674c6951c1
Pawel Wazny <steve91@interia.pl>
2018-11-03 12:21:44 +0100

    sortowanie algorytmu

--- src/SearchAlgorithm.java
+++ src/SearchAlgorithm.java
@@ -1,3 +1,7 @@
-public class SearchAlgorithm {
+import java.util.List;
+
+public interface SearchAlgorithm {
+    Integer search(Integer value, List<Integer> input);
+    Integer search(Integer value, Integer[] input);
 
 }
---
+++ src/SortingAlgorithm.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public interface SortingAlgorithm {
+    List<Integer> sort(List<Integer> input);
+    Integer[] sort(Integer[] input);
+
+}
